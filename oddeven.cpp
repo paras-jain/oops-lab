ed2b8486ac54cab6a5ba28421e18a43100d453e2
Paras Jain <57864667+paras-jain@users.noreply.github.com>
2020-02-23 20:50:32 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Enter the number: ";
+	cin >> a;
+	if(a%2 == 0)
+		cout << "Number is Even";
+	else 
+		cout << "Number is odd";
+	return 0;
+}
